b712e33c2dfd31c32874744878a2dce25560c9da
Junior Loshima <junior.okangondoloshima@uvs.edu.sn>
2020-11-29 17:59:17 +0000

    Step 01 - Initialize a Spring Web Services application with Spring Boot

---
+++ src/main/java/com/ws/soap/webservices/soapcoursemanagement/SoapCourseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ws.soap.webservices.soapcoursemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapCourseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapCourseManagementApplication.class, args);
+	}
+
+}
