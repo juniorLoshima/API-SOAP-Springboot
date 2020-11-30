502cac6e21da66b54aa2ce1db075c347ef55d7f8
Junior Loshima <junior.okangondoloshima@uvs.edu.sn>
2020-11-30 18:09:09 +0000

    Step 12 - Implementing a service - Course Details Service - backend with in memory array list

--- src/main/java/com/ws/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndpoint.java
+++ src/main/java/com/ws/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndpoint.java
@@ -1,9 +1,13 @@
 package com.ws.soap.webservices.soapcoursemanagement.soap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.ws.soap.webservices.soapcoursemanagement.soap.bean.Course;
+import com.ws.soap.webservices.soapcoursemanagement.soap.service.CourseDetailsService;
 
 import https.formation_uvs_sn.courses.CourseDetails;
 import https.formation_uvs_sn.courses.GetCourseDetailsRequest;
@@ -12,6 +16,9 @@ import https.formation_uvs_sn.courses.GetCourseDetailsResponse;
 @Endpoint
 public class CourseDetailsEndpoint {
 	
+	   @Autowired
+	   CourseDetailsService service;
+	   
 	    //method
 		//input - GetCourseDetailsRequest
 		//output - GetCourseDetailsResponse
@@ -23,14 +30,21 @@ public class CourseDetailsEndpoint {
 	public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) 
 	{
 		
-		GetCourseDetailsResponse response = new GetCourseDetailsResponse(); 
-		CourseDetails courseDetails = new CourseDetails();
-		courseDetails.setId (request.getId());
-		courseDetails.setName ("SOA");
-		courseDetails.setDescription("Architecture Orientée Service");
-		response.setCourseDetails(courseDetails);
-		return response; 
+		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+		
+		Course course = service.findById(request.getId());
+		
+		return mapCourse(response, course); 
 		
 	}
+
+		private GetCourseDetailsResponse mapCourse(GetCourseDetailsResponse response, Course course) {
+			CourseDetails courseDetails = new CourseDetails();
+			courseDetails.setId (course.getId());
+			courseDetails.setName (course.getName());
+			courseDetails.setDescription(course.getDescription());
+			response.setCourseDetails(courseDetails);
+			return response;
+		}
 
 }
---
+++ src/main/java/com/ws/soap/webservices/soapcoursemanagement/soap/service/CourseDetailsService.java
@@ -0,0 +1,56 @@
+package com.ws.soap.webservices.soapcoursemanagement.soap.service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.ws.soap.webservices.soapcoursemanagement.soap.bean.Course;
+
+@Component
+public class CourseDetailsService {
+
+	private static List<Course> courses = new ArrayList<>();
+	
+	static {
+		
+		Course course1 = new Course(1, "Ethique d'entreprise", "Autoformation");
+		courses.add(course1);
+		Course course2 = new Course (2, "CSI", "Conception Système Intelligent");
+		courses.add(course2);
+		Course course3 = new Course (3, "IHM", "Interface Homme Macine");
+		courses.add(course3);
+		Course course4 = new Course (4, "PO", "Projet Opérationnel");
+		courses.add(course4);
+		}
+	
+	public Course findById(int id){
+		for(Course course:courses)
+		{
+		if(course.getId()==id)
+		return course;
+		}
+		return null;
+	}
+	
+	public List<Course> findAll()
+	{
+		return courses;
+	}
+	
+	public int deleteById(int id)
+	{
+	Iterator<Course> iterator = courses.iterator();
+	while (iterator.hasNext()){
+		Course course = iterator.next();
+		if(course.getId()==id)
+		{
+		iterator.remove();
+		return 1;
+		}
+		
+	}
+	return 0;
+	}
+}
